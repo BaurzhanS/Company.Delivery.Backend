70381c53e7b106f93640e3a06a2946aa5ca0dd7f
kz.b.sultankulov <kz.b.sultankulov@gmail.com>
2024-07-19 13:56:45 +0500

    Add project files.

---
+++ Company.Delivery.Api/Program.cs
@@ -0,0 +1,17 @@
+﻿using Company.Delivery.Api.AppStart;
+using Company.Delivery.Database;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDeliveryControllers();
+builder.Services.AddDeliveryApi();
+builder.Services.AddDbContext<DeliveryDbContext>(options =>
+                options.UseInMemoryDatabase(databaseName: $"{Database.Name}"));
+
+var app = builder.Build();
+
+app.UseDeliveryApi();
+app.MapControllers();
+
+app.Run();
