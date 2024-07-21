a3cbaa933d0e0429b1962247d064cb5ece080c0a
kz.b.sultankulov <kz.b.sultankulov@gmail.com>
2024-07-21 12:32:57 +0500

    Added repository

---
+++ Company.Delivery.Database/Repos/WaybillRepository.cs
@@ -0,0 +1,37 @@
+﻿using Company.Delivery.Core;
+using Company.Delivery.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company.Delivery.Database.Repos
+{
+    public class WaybillRepository : IWaybillRepository
+    {
+        private readonly DeliveryDbContext _dbContext;
+        public WaybillRepository(DeliveryDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<Waybill> CreateAsync(Waybill waybill, CancellationToken cancellationToken)
+        {
+            await _dbContext.Waybills.AddAsync(waybill, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return waybill;
+        }
+        public async Task DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var waybill = await _dbContext.Waybills.FindAsync(id, cancellationToken);
+            _dbContext.Waybills.Remove(waybill);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+        public async Task<Waybill?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Waybills.FindAsync(id, cancellationToken);
+        }
+        public async Task<Waybill> UpdateByIdAsync(Guid id, Waybill waybill, CancellationToken cancellationToken)
+        {
+            _dbContext.Entry(waybill).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return waybill;
+        }
+    }
+}
--- Company.Delivery.Domain/Interfaces/IWaybillRepository.cs
+++ Company.Delivery.Domain/Interfaces/IWaybillRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Company.Delivery.Domain.Interfaces
+﻿using Company.Delivery.Core;
+
+namespace Company.Delivery.Domain.Interfaces
 {
-    internal interface IWaybillRepository
+    public interface IWaybillRepository
     {
+        Task<Waybill> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<Waybill> CreateAsync(Waybill waybill, CancellationToken cancellationToken);
+        Task<Waybill> UpdateByIdAsync(Guid id, Waybill waybill, CancellationToken cancellationToken);
+        Task DeleteByIdAsync(Guid id, CancellationToken cancellationToken);
     }
 }
